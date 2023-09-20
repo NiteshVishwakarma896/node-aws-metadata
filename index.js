2a6ddaa84f092cede73e8f3277cac4ba57b82c14
NiteshVishwakarma896 <niteshvish896@gmail.com>
2023-09-21 03:55:22 +0530

    modified import and function export

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {getInstanceMetaData} from './lib/AWSMetaData';
+import {getInstanceMetaData} from './lib/AWSMetaData.js';
 
 const response = getInstanceMetaData('ami-id');
 console.log(response)
