9af31bc7b003aa7c3204a6f23aab0cc092d6f4e0
NiteshVishwakarma896 <niteshvish896@gmail.com>
2023-09-21 14:38:04 +0530

    modified import and function export

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {getInstanceMetaData} from './lib/AWSMetaData.js';
-
-const response = getInstanceMetaData('ami-id');
-console.log(response)
+import AWSMetaData from './lib/AWSMetaData.js';
+  
+const awsMetaData = new AWSMetaData() ;
+console.log(awsMetaData.getInstanceMetaData('ami-id'))
