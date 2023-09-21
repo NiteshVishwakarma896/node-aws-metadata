5e832950f2097dba03969cc62d49f835ffafbe7a
NiteshVishwakarma896 <niteshvish896@gmail.com>
2023-09-21 16:08:34 +0530

    modified import and function export

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 import AWSMetaData from './lib/AWSMetaData.js';
   
-async function main() {
-    const awsMetadata = new AWSMetaData(); // Create an instance of AWSMetaData
-
-    try {
-        const metadataValue = await awsMetadata.getInstanceMetaData('instance-id');
-        console.log('Metadata:', metadataValue);
-    } catch (error) {
-        console.error('Error:', error.message);
-    }
-}
-
-main();
+const awsMetaData = new AWSMetaData() ;
+awsMetaData.getInstanceMetaData('ami-id')
+console.log(awsMetaData.metadata)
